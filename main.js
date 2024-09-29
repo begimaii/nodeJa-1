186596d77b1d07be6f809c30a29bf0408d692a9d
Begimai Abdybekova <b.abdybekova@gmail.com>
2024-09-29 13:29:28 -0500

    new changes

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 // const express = require("express");
 import express from "express";
+import { users } from "./data.js";
+
+console.log(users);
 const app = express();
-const port = 3000;
+const port = 4000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send({
+    message: "Hello World!",
+    total_Users: users.length,
+    users: users,
+  });
 });
 
 app.listen(port, () => {
